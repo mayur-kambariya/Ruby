58d392be54d6ebde97f2ebb3750868eadfa13123
mayur-kambariya <mayurkambariya@gmail.com>
2017-01-12 11:43:40 +0530

    rna Transcription Upload 1

---
+++ rna_transcription.rb
@@ -0,0 +1,29 @@
+class Complement
+  def self.of_dna(dna_string)
+    if (dna_string =~ /\A[GCTA]+\z/).nil?
+      return ''
+    else      	
+     replace_string=""
+     dna_array=dna_string.split('')
+     (0...dna_array.length).each do |i|
+       case dna_array[i]
+         when 'G'
+	   replace_string+="C"
+	 when 'C'
+	   replace_string+="G"
+	 when 'T'
+	   replace_string+="A"
+         when 'A'
+	   replace_string+="U" 	
+	 else
+          return ''   
+       end	  
+     end
+     return replace_string
+   end
+  end
+ 	 
+end
+module BookKeeping
+  VERSION=4
+end
