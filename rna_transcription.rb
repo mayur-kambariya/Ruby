f9116be5ceb975f70ffa9fcda65620e01365c06e
mayur-kambariya <mayurkambariya@gmail.com>
2017-01-12 11:57:05 +0530

    Update rna_transcription 2

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -3,26 +3,28 @@ class Complement
     if (dna_string =~ /\A[GCTA]+\z/).nil?
       return ''
     else      	
-     replace_string=""
-     dna_array=dna_string.split('')
-     (0...dna_array.length).each do |i|
-       case dna_array[i]
-         when 'G'
-	   replace_string+="C"
-	 when 'C'
-	   replace_string+="G"
-	 when 'T'
-	   replace_string+="A"
-         when 'A'
-	   replace_string+="U" 	
-	 else
+    	Complement.change_char(dna_string) 
+    end
+  end
+  def self.change_char(dna_string)
+    replace_string=""
+    dna_array=dna_string.split('')
+    (0...dna_array.length).each do |i|
+      case dna_array[i]
+        when 'G'
+	  replace_string+="C"
+	when 'C'
+	  replace_string+="G"
+	when 'T'
+	  replace_string+="A"
+        when 'A'
+	  replace_string+="U" 	
+	else
           return ''   
        end	  
      end
      return replace_string
-   end
-  end
- 	 
+  end 
 end
 module BookKeeping
   VERSION=4
