e0ed2460a24901ca25eca0bf8f86790ce3e420e2
mayur-kambariya <mayurkambariya@gmail.com>
2017-01-20 15:41:58 +0530

    binary number update 2

--- binary/binary.rb
+++ binary/binary.rb
@@ -1,16 +1,16 @@
 class Binary
   def self.to_decimal(binary_string)
     if (binary_string =~ /^[01]+$/)
-      binary_string_number=binary_string.to_i
-      sum=0
-      i=0
+      binary_string_number = binary_string.to_i
+      sum = 0
+      i = 0
       while binary_string_number != 0
-        digit=binary_string_number%10
-        if(digit!=0)
-          sum+=(digit*(2**i)).to_i
+        digit = binary_string_number % 10
+        if(digit != 0)
+          sum += (digit *(2 ** i)).to_i
         end
-        i+=1
-        binary_string_number/=10
+        i += 1
+        binary_string_number /= 10
       end
       return sum
     else
@@ -19,5 +19,5 @@ class Binary
   end
 end
 module BookKeeping
- 	VERSION=3
+ 	VERSION = 3
 end
